a228bf1ef90dfabba1e4497a7b489ca198d3dd99
bagdoc <waneanthony25@gmail.com>
2026-09-18 19:47:31 +0800

    added bagdoc profile

---
+++ app/src/test/java/com/example/repov1/quarter2nd/bagdocminipeta.java
@@ -0,0 +1,22 @@
+package com.example.repov1.quarter2nd;
+
+import org.junit.Test;
+
+public class bagdocminipeta {
+    @Test
+    public void thisisMyInfo() {
+
+        //--- 1. THE INPUT (Storing your personal details in variables) -
+        String myName = "Anthony wane bagdoc";
+        String favGame = "valorant";
+        String favFood = "Chicken Breast";
+        int Age = 17;
+
+// 2. THE OUTPUT (Printing to the console)
+        System.out.println("----- MY INFORMATION -----");
+        System.out.println("Hello, I am "+myName +" currently, "+Age +"17 years old.");
+        System.out.println("My favorite game to play is "+favGame +".");
+        System.out.println("playing basketball, "+favFood +" gets me through the day!");
+
+    }
+}
