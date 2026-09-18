d590e6d8ebf377bf8904a75df5117638028cb3ae
GittingAngry <onestep2correction@gmail.com>
2026-09-19 01:39:32 +0800

    "Added Shem Yves B. Soriano profile test"

---
+++ app/src/test/java/com/example/repov1/quarter2nd/sorianominipeta.java
@@ -0,0 +1,21 @@
+package com.example.repov1.quarter2nd;
+
+import org.junit.Test;
+public class sorianominipeta {
+    @Test
+    public void printMyProfile() {
+        String myName = "Shem Yves B. Soriano";
+        String favHobby = "Listening to music";
+        String favFood = "Tinola";
+        int myAge = 17;
+
+        System.out.println("--- My Digital Profile ---");
+        System.out.println("Hello!, my name is " + myName + " and i am " + myAge);
+        System.out.println("I usually spend my time " + favHobby + ".");
+        System.out.println("And one of my favorite food is " + favFood + ".");
+    }
+
+
+
+}
+
